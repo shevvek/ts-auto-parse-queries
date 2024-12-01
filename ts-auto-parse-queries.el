b7ee104012ac6465c0db1a20829377d0c8ff0174
Saul James Tobin <saul.james.tobin@gmail.com>
2024-11-30 19:05:04 -0500

    Mostly working script to parse .scm query files

---
+++ ts-auto-parse-queries.el
@@ -0,0 +1,72 @@
+;; -*- lexical-binding: t -*-
+
+(defvar query-specs
+  '(":language lilypond\n"
+    ":feature auto\n"
+    "("))
+
+(defun insert-query-specs ()
+  ;; Move from end of last sexp to beginning of next sexp
+  (forward-sexp)
+  (when (thing-at-point 'sexp)
+    (backward-sexp)
+    (apply #'insert query-specs)))
+
+(defun format-pred ()
+  ;; Assume point is just after #
+  (delete-char -1)
+  (insert ":")
+  (forward-sexp)
+  (delete-char 1))
+
+(defun translate-match-query ()
+  ;; Assume the point is just after :match
+  (forward-sexp)
+  (transpose-sexps 1)
+  (backward-sexp 2)
+  (mark-sexp 1)
+  ;; The region should now be exactly the regex string
+  (replace-region-contents (point) (mark)
+                           (lambda ()
+                             (prin1-to-string
+                              (rxt-pcre-to-rx
+                               (buffer-substring (point) (mark)))))))
+
+(defun handle-query-predicate ()
+  (let ((pred (thing-at-point 'word)))
+    (cond
+     ((equal "match" pred)
+      (format-pred)
+      (translate-match-query))
+     ((equal "equal" pred)
+      (format-pred))
+     ((equal "pred" pred)
+      (format-pred)
+      (display-warning 'ts-query-translate
+                       "Custom predicates must be manually translated"))
+     (t ;; else
+      (backward-up-list)
+      (kill-sexp)))))
+
+(defun translate-ts-query-file (scm-file out-file)
+  (with-temp-buffer
+    (insert-file-contents scm-file)
+    (goto-char (point-min))
+    (insert-query-specs)
+    (while (not (eobp))
+      (forward-sexp)
+      (when (eolp)
+        (insert ")")
+        (insert-query-specs)))
+    (goto-char (point-max))
+    (while (re-search-backward (rx (seq (group space) "." (group space)))
+                               nil t)
+      (replace-match (rx (seq (backref 1) ":anchor" (backref 2)))))
+    (goto-char (point-min))
+    (while (search-forward "(#" nil t)
+      (handle-query-predicate))
+    (write-file out-file)))
+
+(translate-ts-query-file
+ "~/Documents/tree-sitter-lilypond/queries/highlights.scm"
+ "~/.emacs.d/lilypond-ts/auto-ly-highlights.el")
